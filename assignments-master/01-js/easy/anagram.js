c61850ca6414b6f5432e26a997bb54d049468f61
srikrishnanaik <srikrishnanaikb@gmail.com>
2023-12-21 10:23:37 +0530

    Javascript

--- assignments-master/01-js/easy/anagram.js
+++ assignments-master/01-js/easy/anagram.js
@@ -5,7 +5,31 @@
 */
 
 function isAnagram(str1, str2) {
+  const str1Arr = str1.split("");
+  const str2Arr = str2.split("");
+  if (str1Arr.length === str2Arr.length ) {
+    for (let i = 0; i <= str1Arr.length; i++) {
+      for(let j = 0; j <= str2Arr.length; j++ ){
+      if(str1Arr[i] != str2Arr[j]) {
+        // console.log(str1[i] + str2[j])
+        continue;
+      } else if(str1Arr[i] === str2Arr[j]) {
+        str1Arr.pop(str1Arr[i])
+        str2Arr.pop(str2Arr[j])
+        console.log(str1Arr+" "+str2Arr)
 
+      }  else {
+        console.log("Strings are not Anagram")
+      }
+    }
+    }
+    if(str1Arr.length == 0 && str2Arr.length == 0) {
+      console.log("Strings are Anagram");
+    }
+  } else {
+    console.log("Strings are Not Anagram");
+  }
 }
 
+isAnagram("help", "pelh")
 module.exports = isAnagram;
