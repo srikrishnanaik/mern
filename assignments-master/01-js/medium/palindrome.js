05f2ea37c9226ab44db6bd0064abaae2659f5caf
srikrishnanaik <srikrishnanaikb@gmail.com>
2023-12-22 14:43:09 +0530

    Javascript

--- assignments-master/01-js/medium/palindrome.js
+++ assignments-master/01-js/medium/palindrome.js
@@ -4,7 +4,19 @@
 */
 
 function isPalindrome(str) {
-  return true;
+  input = str.toLowerCase().replace(/[^a-zA-Z ]/g, "").replaceAll(/\s/g,'');
+  strCheck = input.split("");
+  console.log(strCheck);
+  let j = strCheck.length-1;
+  for(let i=0; i <= Math.floor(strCheck.length/2); i++){
+  if(strCheck[i] == strCheck[j]) {
+    console.log(strCheck[i]+" "+strCheck[j]);
+    j--;
+  } else{
+    return false
+  }
+  }
+  return true
 }
-
+console.log(isPalindrome("Eva, can I see bees in a cave?"));
 module.exports = isPalindrome;
