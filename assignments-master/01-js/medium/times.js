cbcde6f5415ad624da7d61b863805a35dbe5db48
srikrishnanaik <srikrishnanaikb@gmail.com>
2023-12-22 16:34:05 +0530

    Javascript

--- assignments-master/01-js/medium/times.js
+++ assignments-master/01-js/medium/times.js
@@ -9,5 +9,13 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let start = Date.now();
+    for(let i=0; i <=n; i++){
+
+    }
+    let end = Date.now()
+    return end-start;
 }
+
+
+console.log("Time taken "+calculateTime(1000000000)+" milliseconds")
