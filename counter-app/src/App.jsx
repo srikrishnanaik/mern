f52f8d20ace8c770c6b8a821c569adaec0b99837
srikrishnanaik <srikrishnanaikb@gmail.com>
2023-12-31 16:41:30 +0530

    Javascript

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+  function onClickHandler() {
+     setCount(count + 1);
+  }
+  return (
+      <div>
+       <button onClick={onClickHandler}>Counter {count}</button>
+      </div>
+      
+  )
+}
+
+export default App
